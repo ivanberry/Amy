e17a151b4617491a7d42a42b823c10c96f481e54
ivanberry <tabvim.dev@gmail.com>
2017-11-20 07:08:14 +0800

    (.location) Add test utils

---
+++ react-backend/test/utils.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const config = require('../config');
+const mongoose = require('mongoose');
+
+process.env.NODE_ENV = 'test';
+
+beforeEach(done => {
+	function clearDB() {
+		for (let collection in mongoose.connection.collections) {
+			mongoose.connection.collections[collection].remove();
+		}
+		return done();
+	}
+
+	if (mongoose.connection.readyState === 0) {
+		mongoose.connect(config.test, err => {
+			if (err) throw new Error(err);
+			return clearDB();
+		});
+	} else {
+		return clearDB();
+	}
+});
+
+afterEach(done => {
+	mongoose.disconnect();
+	return done();
+});
