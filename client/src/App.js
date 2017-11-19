219158fac137171f700ce80eab66bb1c7f6ba0f5
ivanberry <tabvim.dev@gmail.com>
2017-11-20 05:48:03 +0800

    (.all) Add eslint and prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-  state = { users: [] };
-  componentDidMount() {
-    fetch("/api/users")
-      .then(res => res.json())
-      .then(users => this.setState({ users }))
-      .catch(err => console.log(err));
-  }
+	state = { users: [] };
+	componentDidMount() {
+		fetch('/api/users')
+			.then(res => res.json())
+			.then(users => this.setState({ users }))
+			.catch(err => console.log(err));
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Users</h1>
+				{this.state.users.map(user => <div key={user.id}>{user.username}</div>)}
+			</div>
+		);
+	}
 }
 
 export default App;
--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,22 +1,22 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.json([
-    {
-      id: 1,
-      username: "tab"
-    },
-    {
-      id: 2,
-      username: "shirting"
-    },
-    {
-      id: 3,
-      username: "amy"
-    }
-  ]);
+router.get('/', function(req, res) {
+	res.json([
+		{
+			id: 1,
+			username: 'tab'
+		},
+		{
+			id: 2,
+			username: 'shirting'
+		},
+		{
+			id: 3,
+			username: 'amy'
+		}
+	]);
 });
 
 module.exports = router;
