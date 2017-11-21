18ade83979d9daf09c7caccdd49e89c767468b93
ivanberry <tabvim.dev@gmail.com>
2017-11-22 06:37:53 +0800

    (.location) Add users post verb test

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../config/config').db;
+
+var UserModel = require('../model/User');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+	let users = [];
 	let response = {
 		statusCode: 200,
-		message: 'There are all users!',
-		data: []
+		message: 'Success!',
+		data: users
 	};
 	res.statusCode = 200;
 	res.json(response);
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
 	res.end('wowo');
 });
 
--- react-backend/test/user-api-test.js
+++ react-backend/test/user-api-test.js
@@ -21,10 +21,36 @@ describe('User API Test', () => {
 			.then(res => {
 				res = JSON.parse(res);
 				except(res.statusCode, 'statueCode').to.be.equal(200);
+				except(res.message).to.be.equal('Success!');
 				except(res.data, 'data').to.be.an('array');
 				done();
 			})
-			.catch(err => done(err));
+			.catch(err => {
+				throw new Error(err);
+			});
+	});
+
+	it('Get not empty user list', done => {
+		let newUser = new User({
+			name: 'tab',
+			password: 'tab'
+		});
+		newUser.createUser(err => {
+			if (err) throw new Error(err);
+			rp(url)
+				.then(res => {
+					res = JSON.parse(res);
+					except(res.statusCode, 'statusCode').to.be.equal(200);
+					except(res.message, 'message').to.be.equal('Success!');
+					except(res.data, 'data')
+						.to.be.an('array')
+						.with.lengthOf(1);
+					done();
+				})
+				.catch(err => {
+					throw new Error(err);
+				});
+		});
 	});
 
 	it('Register a new user', done => {
@@ -40,13 +66,13 @@ describe('User API Test', () => {
 
 		rp(options)
 			.then(function(res) {
-                //what did you except?
+				//what did you except?
 				// POST succeeded...
 				console.log(res);
 				done();
 			})
 			.catch(err => {
-                throw new Error(err);
+				throw new Error(err);
 			});
 	});
 });
