8375b4d0671fd5cdd464b508224d68f5fafba4bc
ivanberry <tabvim.dev@gmail.com>
2017-12-02 09:07:02 +0800

    Update Basic auth

--- react-backend/lib/basic-auth.js
+++ react-backend/lib/basic-auth.js
@@ -1,16 +1,17 @@
+const bcrypt = require('bcrypt');
 const UserModel = require('../model/User');
 
 /**
  * Authorization only
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  */
 module.exports = function auth(req, res, next) {
-	let _auth64base = (req.headers.authorization || '').split(' ') || '';
-	const [name, hash] = new Buffer(_auth64base, 'base64').toString().split(':');
+	let _auth64base = (req.headers.authorization || '').split(' ')[1] || '';
+	const [name, password] = new Buffer(_auth64base, 'base64').toString().split(':');
 
-	if (!name || !hash) {
+	if (!name || !password) {
 		res.status(401);
 		res.set('WWW-Authenticate', 'Basic realm="Resource Protected"');
 		res.json({
@@ -21,17 +22,20 @@ module.exports = function auth(req, res, next) {
 		//forward to next
 		UserModel.findOne({ name: name }, 'hash', { lean: true }, (err, doc) => {
 			if (err) throw new Error(err);
-			if (hash !== doc.hash) {
-				res.status(401);
-				res.set('WWW-Authenticate', 'Basic realm="Resource Protected"');
-				res.json({
-					statusCode: 401,
-					message: 'User Unauthorized!'
-				});
-			} else {
-				res.status(200);
-				next();
-			}
+			bcrypt.compare(password, doc.hash, (err, result) => {
+				if (err) next(err);
+				if (result) {
+					res.status(200);
+					next();
+				} else {
+					res.status(401);
+					res.set('WWW-Authenticate', 'Basic realm="Resource Protected"');
+					res.json({
+						statusCode: 401,
+						message: 'User Unauthorized!'
+					});
+				}
+			});
 		});
 	}
 };
