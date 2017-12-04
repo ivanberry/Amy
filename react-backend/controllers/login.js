25d526443aac297a3973987febd5cbd2f8d59186
ivanberry <tabvim.dev@gmail.com>
2017-12-04 19:44:33 +0800

    Fix (.all) Login logic

--- react-backend/controllers/login.js
+++ react-backend/controllers/login.js
@@ -3,25 +3,33 @@ const bcrypt = require('bcrypt');
 
 exports.login = function(req, res, next) {
 	let { name, password } = req.body;
-	UserModel.findOne({ name: name }, 'hash', { lean: true }, (err, doc) => {
-		if (err) next(err);
-		bcrypt.compare(password, doc.hash, (err, isUser) => {
-			if (err) next(err);
-			if (isUser) {
-				req.session.user = name;
-				res.status(200);
-				res.json({
-					statusCode: 200,
-					message: 'Login Successed!'
-				});
-				next();
-			} else {
-				res.status(200);
-				res.json({
-					statusCode: 200,
-					message: 'You need login!'
-				});
-			}
+	if (!name || !password) {
+		res.status(401);
+		res.json({
+			statusCode: 401,
+			message: 'Params Error'
 		});
-	});
+	} else {
+		UserModel.findOne({ name: name }, 'hash', { lean: true }, (err, doc) => {
+			if (err) next(err);
+			bcrypt.compare(password, doc.hash, (err, isUser) => {
+				if (err) next(err);
+				if (isUser) {
+					req.session.user = name;
+					res.status(200);
+					res.json({
+						statusCode: 200,
+						message: 'Login Successed!'
+					});
+					next();
+				} else {
+					res.status(200);
+					res.json({
+						statusCode: 200,
+						message: 'You need login!'
+					});
+				}
+			});
+		});
+	}
 };
