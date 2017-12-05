32f35ad2c2401822077eb498ca168e63b71d5ec0
ivanberry <tabvim.dev@gmail.com>
2017-12-05 13:09:24 +0800

    Add addtional test cases

--- react-backend/test/post-model-test.js
+++ react-backend/test/post-model-test.js
@@ -24,19 +24,64 @@ describe('Post Collection Tests', () => {
 		let newPost = new Post(_p);
 		newPost
 			.createPost()
-            .then(post => {
-                expect(post._id).not.null;
-                expect(post.author).to.be.a('string').to.be.equal(_p.author);
-                expect(post.title).to.be.a('string').to.be.equal(_p.title);
-                expect(post.body).to.be.a('string').to.be.equal(_p.body);
-                expect(post.tag).to.be.a('string').to.be.equal('Default');
-                expect(post.modifyAt).to.be.a('date').not.null;
-                expect(post.createAt).to.be.a('date').not.null;
-                expect(post.viewCounter).to.be.a('number');
-                done();
-            })
-            .catch(err => {
-                done(err);
-            });
+			.then(post => {
+				expect(post._id).not.null;
+				expect(post.author)
+					.to.be.a('string')
+					.to.be.equal(_p.author);
+				expect(post.title)
+					.to.be.a('string')
+					.to.be.equal(_p.title);
+				expect(post.body)
+					.to.be.a('string')
+					.to.be.equal(_p.body);
+				expect(post.tag)
+					.to.be.a('string')
+					.to.be.equal('Default');
+				expect(post.modifyAt).to.be.a('date').not.null;
+				expect(post.createAt).to.be.a('date').not.null;
+				expect(post.viewCounter).to.be.a('number');
+				done();
+			})
+			.catch(err => {
+				done(err);
+			});
 	});
+
+	it('Create Post without title', done => {
+		let newPost = new Post({
+			body: 'body',
+			author: 'tab'
+		});
+
+		newPost
+			.createPost()
+			.then(post => {
+				done();
+			})
+			.catch(err => {
+                let _err = err.errors.title;
+				expect(_err.name).to.be.equal('ValidatorError');
+				expect(_err.message).to.be.equal('Path `title` is required.');
+				done();
+			});
+    });
+    
+    it('Create new Post without body', done => {
+        let newPost = new Post({
+            title: 'body',
+            author: 'tab'
+        });
+
+        newPost
+            .createPost()
+            .then(post => {
+                done();
+            }).catch(err => {
+                let _err = err.errors.body;
+                expect(_err.name).to.be.equal('ValidatorError');
+                expect(_err.message).to.be.equal('Path `body` is required.');
+                done();
+            });
+    });
 });
