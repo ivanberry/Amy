cd5b80174aed08bf6c3e0270aa23bf597a170d8d
ivanberry <tabvim.dev@gmail.com>
2017-12-08 07:05:24 +0800

    Add redux logger

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { homeReducer } from './reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import registerServiceWorker from './registerServiceWorker';
+const store = createStore(homeReducer, applyMiddleware(logger));
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
