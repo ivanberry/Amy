7b85db84a708019d63570afef7f0e9451f400bcd
ivanberry <tabvim.dev@gmail.com>
2017-12-13 07:17:41 +0800

    Implement logout flow with saga

--- client/src/saga/saga.js
+++ client/src/saga/saga.js
@@ -1,6 +1,6 @@
 import * as ActionTypes from '../constants/actionTypes';
 import { setAuthState, sendingRequest } from '../actions/actions';
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery, fork } from 'redux-saga/effects';
 import axios from 'axios';
 
 /**
@@ -8,17 +8,21 @@ import axios from 'axios';
  * @param {*} name
  * @param {*} password
  */
-function loginI(name, password) {
+function _logIn(name, password) {
 	return axios.post('/api/login', {
 		name,
 		password
 	});
 }
 
+function _logOut() {
+	return axios.get('/api/logout');
+}
+
 function* loginFlow(action) {
 	yield put(sendingRequest(true));
 	try {
-		const result = yield call(loginI, action.user.username, action.user.password);
+		const result = yield call(_logIn, action.user.username, action.user.password);
 		let isAuth = false;
 		if (result.status === 200 && result.data.message === 'Login Successed!') {
 			isAuth = true;
@@ -31,8 +35,26 @@ function* loginFlow(action) {
 	}
 }
 
+function* logoutFlow() {
+	yield put(sendingRequest(true));
+	try {
+		const result = yield call(_logOut);
+		let isAuth = true;
+		if (result.status === 200) {
+			isAuth = false;
+		}
+		yield put(setAuthState(isAuth));
+		yield put(sendingRequest(false));
+		
+	} catch (e) {
+		yield put({type: 'LOGOUT_IN_FAIL', message: e.message});
+		yield put(sendingRequest(false));
+	}
+}
+
 function* mySaga() {
 	yield takeEvery(ActionTypes.LOGIN_REQUEST, loginFlow); //subscribe the LOGIN_IN action
+	yield takeEvery(ActionTypes.LOGOUT, logoutFlow); //subscribe the LOGIN_IN action
 }
 
 export default mySaga;
