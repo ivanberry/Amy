8c8fdcfe1f80bd7706164f753eecdf1fbb250bc4
ivanberry <tabvim.dev@gmail.com>
2017-12-16 08:56:41 +0800

    Implement Article List component

--- client/src/components/common/List.js
+++ client/src/components/common/List.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import moment from 'moment';
+
 export const List = (post) => {
-	let { title, body, author, views } = post;
+	let { title, body, author, viewCounter, tag, createdAt, updatedAt } = post;
+	moment(createdAt).format('YYYY-MM-DD: HH-MM');
+
 	return (
 		<li>
 			<h2>{title}</h2>
 			<p>{body}</p>
 			<div>
-				<span>{author}</span>
-				<span>{views}</span>
+				<span>作者: {author} </span>
+				<span>类别: {tag} </span>
+				<span>阅读数: {viewCounter} </span>
+				<span>创建于: {moment(createdAt).format('YYYY-MM-DD')} </span>
+				<span>上次更新: {moment(updatedAt).format('YYYY-MM-DD')} </span>
 			</div>
 		</li>
 	);
 };
 
 List.propTypes = {
-	post: PropTypes.object
+	post: PropTypes.array
 };
 
 export default List;
