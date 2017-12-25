d7297ce41cc0ade74d4a25a7ceab3b4c5c54a48b
ivanberry <tabvim.dev@gmail.com>
2017-12-25 09:33:27 +0800

    Implement user login

--- react-backend/controllers/login.js
+++ react-backend/controllers/login.js
@@ -12,33 +12,34 @@ exports.login = function(req, res, next) {
 	} else {
 		UserModel.findOne({ name: name }, 'hash', { lean: true }, (err, doc) => {
 			if (err) next(err);
-			let _id = doc._id;
 			if (!doc) {
 				res.status(401);
 				res.json({
 					statusCode: 401,
 					message: 'User Not Exists!'
 				});
+			} else {
+				let _id = doc._id;
+				bcrypt.compare(password, doc.hash, (err, isUser) => {
+					if (err) next(err);
+					if (isUser) {
+						req.session.user = name;
+						req.session.userId = _id;
+						res.status(200);
+						res.json({
+							statusCode: 200,
+							message: 'Login Successed!'
+						});
+						next();
+					} else {
+						res.status(200);
+						res.json({
+							statusCode: 200,
+							message: 'You need login!'
+						});
+					}
+				});
 			}
-			bcrypt.compare(password, doc.hash, (err, isUser) => {
-				if (err) next(err);
-				if (isUser) {
-					req.session.user = name;
-					req.session.userId = _id;
-					res.status(200);
-					res.json({
-						statusCode: 200,
-						message: 'Login Successed!'
-					});
-					next();
-				} else {
-					res.status(200);
-					res.json({
-						statusCode: 200,
-						message: 'You need login!'
-					});
-				}
-			});
 		});
 	}
 };
