748896bcecb5edb17e02aa0c5451ce7944095f46
ivanberry <tabvim.dev@gmail.com>
2017-12-27 19:32:30 +0800

    Add simpliest CORS setup

--- react-backend/app.js
+++ react-backend/app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var cors = require('cors');
 
 let secret = {
 	secret: 'keyboard cat',
@@ -13,8 +14,6 @@ let secret = {
 		maxAge: 600000
 	}
 };
-
-// var cors = require('cors');
 
 const mongoose = require('mongoose');
 
@@ -39,7 +38,7 @@ if(app.get('env') === 'production') {
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-// app.use(cors);
+app.use(cors());
 app.use(session(secret));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
