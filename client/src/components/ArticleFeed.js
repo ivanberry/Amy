1a12798fc27a69cf9a836f0f9d640cf6aba25e22
ivanberry <tabvim.dev@gmail.com>
2017-12-28 13:03:52 +0800

    Add Index Article Feeds

---
+++ client/src/components/ArticleFeed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Article from './Article';
+
+class ArticleFeed extends Component {
+
+	render() {
+		return <Article />;
+	}
+}
+
+export default ArticleFeed;
