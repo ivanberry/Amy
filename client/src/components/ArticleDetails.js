469bc1ad3e12e3ce2f86113ceddafe57f2274249
ivanberry <tabvim.dev@gmail.com>
2018-01-04 21:28:16 +0800

    Implement article detail

--- client/src/components/ArticleDetails.js
+++ client/src/components/ArticleDetails.js
@@ -4,19 +4,56 @@ import { connect } from 'react-redux';
 class ArticleDetails extends Component {
 	constructor(props) {
 		super(props);
+		let _articles = props.articles;
 		this.state = {
-			article: this.props.articles
+			article: this.refactorArticle(_articles)
 		};
 	}
 
+	refactorArticle(articles) {
+		let id = this.props.location.hash.split('#')[1],
+			_article = { id: id };
+		articles
+			? articles.forEach(article => {
+					let _id = article._id;
+					if (_id === id) {
+						let article_details;
+						({ _id, ...article_details } = article);
+						_article = {
+							id: id,
+							article_details: article_details
+						};
+					}
+				})
+			: [];
+		return _article;
+	}
+
 	render() {
-		return <span>xxx</span>;
+		let article = this.state.article;
+		let { title, body, viewCounter, tag, createdAt, updatedAt } = article.article_details;
+
+		return (
+			<div>
+				<h2>{title}</h2>
+				<hr />
+				<p>{body}</p>
+				<ul>
+					<li>{updatedAt}</li>
+					<li>{createdAt}</li>
+					<li>{viewCounter}</li>
+					<li>{createdAt}</li>
+					<li>{viewCounter}</li>
+					<li>{tag}</li>
+				</ul>
+			</div>
+		);
 	}
 }
 
 function mapStateToProps(state) {
 	return {
-        articles: state.articles,
+		articles: state.articles
 	};
 }
 
