8c43dcef56641adef01f3694f6a0bf1507048754
ivanberry <tabvim.dev@gmail.com>
2018-01-16 06:40:26 +0800

    Add Article ref and test case

--- server/shemas/Tag.js
+++ server/shemas/Tag.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-module.exports = new Schema(
-	{
-		tag: {
-			type: String,
-      default: 'Default',
-      required: true
-		}
-	},
-	{
-		timestamps: true
-	}
-);
+module.exports = new Schema({
+	tag: {
+		type: String,
+		default: 'Default',
+		required: true
+  },
+  articles: {
+    type: Array,
+    ref: 'ArtileModel'
+  }
+});
