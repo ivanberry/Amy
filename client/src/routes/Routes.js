1f056124ada7bbe0527b1c8df09b79be1ebcfb9e
ivanberry <tabvim.dev@gmail.com>
2018-01-24 07:39:11 +0800

    feat(all) Update Routes with TypeArticleFeed

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -3,18 +3,19 @@ import { Route } from 'react-router-dom';
 
 import LoginForm from '../components/Login';
 import Dashboard from '../components/Dashboard';
-import AllArticleFeed from '../components/AllArticleFeed';
+import HomeArticleFeed from '../components/HomeArticleFeed';
 import ArticleDetailContainer from '../container/ArticleDetailContainer';
+import TypeArticleFeed from '../components/TypeArticleFeed';
 import PrivateRoute from '../components/PrivateRoute';
 
 class Routes extends Component {
 	render() {
 		return (
 			<div>
-				<Route exact path="/" component={AllArticleFeed} />
+				<Route exact path="/" component={HomeArticleFeed} />
 				<Route path="/login" component={LoginForm} />
 				<Route path="/article/:id" component={ArticleDetailContainer} />
-				<Route path="/t_articles/:tag" component={AllArticleFeed} />
+				<Route path="/t_articles/:tag" component={TypeArticleFeed} />
 				<PrivateRoute exact path="/dashboard" component={Dashboard} />
 			</div>
 		);
