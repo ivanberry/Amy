bb73b43013b42cdbf5391f3b9326a3ee990f1cc5
ivanberry <tabvim.dev@gmail.com>
2018-01-26 18:01:03 +0800

    .fix(location) implement component HTML

--- client/src/container/ArticleNav/ArticleNavContainer.js
+++ client/src/container/ArticleNav/ArticleNavContainer.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTyps from 'prop-types';
+
 import ArticleNavItem from '../../components/ArticleNavItem';
 import styles from './ArticleNav.module.css';
 import { getTagsRequest } from '../../actions/actions';
 
 class ArticleNavContainer extends Component {
-
 	componentDidMount() {
 		this.props.dispatch(getTagsRequest());
 	}
 
 	// fetch all tag
 	render() {
-		return <div className={styles.container}>{this.props.tags.map(tag => <ArticleNavItem key={tag.name} tag={tag.name} />)}</div>;
+		return (
+				<div className={styles.container}>
+					{this.props.tags.map(tag => <ArticleNavItem key={tag.name} tag={tag.name} />)}
+				</div>
+		);
 	}
 }
 
