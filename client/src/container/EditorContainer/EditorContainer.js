e342f7ed00c56e0e6c86fa2270392811697dcb8f
ivanberry <tabvim.dev@gmail.com>
2018-01-30 06:30:02 +0800

    Push history props down

--- client/src/container/EditorContainer/EditorContainer.js
+++ client/src/container/EditorContainer/EditorContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Editor from '../../components/common/Editor/Editor';
 
 class EditorContainer extends Component {
 	render() {
-		return <Editor />;
+		return <Editor history={this.props.history} />;
 	}
 }
+
+EditorContainer.propTypes = {
+	history: PropTypes.object
+};
 
 export default EditorContainer;
