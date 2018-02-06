7b4a6c7c0614f6112de3fecf9f0fb7cbc5accf3a
ivanberry <tabvim.dev@gmail.com>
2018-02-07 06:51:04 +0800

    Fetch tags in Editor container

--- client/src/container/EditorContainer/EditorContainer.js
+++ client/src/container/EditorContainer/EditorContainer.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Editor from '../../components/common/Editor/Editor';
 
+import { getTagsRequest } from '../../actions/actions';
+
 class EditorContainer extends Component {
+
+	componentDidMount() {
+		this.props.dispatch(getTagsRequest());
+	}
+
 	render() {
-		return <Editor history={this.props.history} />;
+		return <Editor history={this.props.history} tags={this.props.tags} />;
 	}
 }
 
 EditorContainer.propTypes = {
-	history: PropTypes.object
+	history: PropTypes.object,
+	tags: PropTypes.array,
+	dispatch: PropTypes.func
 };
 
-export default EditorContainer;
+function mapStateToProps(state) {
+	return {
+		tags: state.tags
+	};
+}
+
+export default connect(mapStateToProps)(EditorContainer);
