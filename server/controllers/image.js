8b6077b0705a06e48018a3e693b68f101adcc52f
ivanberry <tabvim.dev@gmail.com>
2018-03-07 09:35:25 +0800

    Finishi image paste to upload and response with md

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -5,41 +5,53 @@ const formidable = require('formidable');
 const UPLOADS = path.join(__dirname, '..', `uploads`);
 
 function postImage(req, res, next) {
-    let form = new formidable.IncomingForm();
-    form.uploadDir = UPLOADS;
-    form.keepExtensions = true;
+	let form = new formidable.IncomingForm();
+	form.uploadDir = UPLOADS;
+	form.keepExtensions = true;
+	let response = {
+		statusCode: 500,
+		message: 'Server error'
+	};
 
-    form.parse(req, function (err, fields, files) {
-        if (err) {
-            res.status(500);
-            res.end('Server error');
-        } else {
-            let image = files['image'],
-                md_path = `![${image.name}](${image.path})`;
-            res.end(md_path);
-        }
-    });
+	form.on('file', (name, file) => {
+		fs.rename(file.path, form.uploadDir + '/' + file.name);
+	});
 
-    return;
+	form.on('error', err => {
+		res.json(response);
+	});
+
+	form.parse(req, function(err, fields, files) {
+		if (err) {
+			res.status(500);
+			res.end('Server error');
+		} else {
+			let image = files['image'],
+				md_path = `![${image.name}](/api/image/${image.name})`;
+			res.end(md_path);
+		}
+	});
+
+	return;
 }
 
 function getImage(req, res, next) {
-    let { image } = req.params;
-    let response = {
-        statusCode: 404,
-        message: 'Param error'
-    };
-    if (!image) {
-        res.json(response);
-    }
+	let { image } = req.params;
+	let response = {
+		statusCode: 404,
+		message: 'Param error'
+	};
+	if (!image) {
+		res.json(response);
+	}
 
-    let _readStream = fs.createReadStream(path.join(UPLOADS, `${image}`));
-    _readStream.pipe(res);
-    _readStream.on('error', (err) => {
-        response.message = 'Image not Found!';
-        res.json(response);
-        next(err);
-    });
+	let _readStream = fs.createReadStream(path.join(UPLOADS, `${image}`));
+	_readStream.pipe(res);
+	_readStream.on('error', err => {
+		response.message = 'Image not Found!';
+		res.json(response);
+		next(err);
+	});
 }
 
 exports.postImage = postImage;
