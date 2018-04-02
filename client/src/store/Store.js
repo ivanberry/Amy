44f1796d0ce0e419abf2709e0c3cec8e858b78ac
ivanberry <tabvim.dev@gmail.com>
2018-04-02 11:38:16 +0800

    Add Redux dev tool

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -3,11 +3,11 @@ import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 import { homeReducer } from '../reducers/reducers';
-import  mySaga  from '../saga/saga';
+import mySaga from '../saga/saga';
 
 const mySagaMiddle = createSagaMiddleware();
 let middleware = [mySagaMiddle, logger];
-const store = createStore(homeReducer, applyMiddleware(...middleware));
+const store = createStore(homeReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middleware));
 
 mySagaMiddle.run(mySaga);
 
